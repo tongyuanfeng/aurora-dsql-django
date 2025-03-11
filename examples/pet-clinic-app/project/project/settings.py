14394cff7d74f42b46d08eb35467867481ceb3fa
同元峰 <tong_yuanfeng@163.com>
2025-03-11 15:55:58 +0800

    Update settings.py
    根据源码 tests--test_settings.py 
    修复频繁掉线问题

--- examples/pet-clinic-app/project/project/settings.py
+++ examples/pet-clinic-app/project/project/settings.py
@@ -119,3 +119,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Disable Django's transaction management
+DISABLE_TRANSACTION_MANAGEMENT = True 
